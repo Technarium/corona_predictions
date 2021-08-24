e461501f58b8ef076b64c19716d5a21196fa1dc4
Fast Forward <ffwd-txnx@users.noreply.github.com>
2021-08-24 11:25:34 +0300

    Update cases for August 24
    Source:
    
    https://www.lrt.lt/naujienos/lietuvoje/2/1474417/lietuvoje-patvirtinta-600-koronaviruso-atveju-mire-1-zmogus-per-para-paskiepyta-daugiau-nei-15-tukst-asmenu

--- corona_predictions.py
+++ corona_predictions.py
@@ -101,7 +101,7 @@ DAILY_CASES = [
     212, 394, 503, 462, 528, 496, 367,
     287, 517, 576, 599, 548, 585, 434,
     356, 560, 576, 506, 537, 580, 407,
-    314,
+    314, 600,
 ]
 # previously, this calculation was done manually; an artifact of better days
 cumulative = 0
