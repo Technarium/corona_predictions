7a3ce2f5faa40ffec39703b4a26b074ff4ca2bf1
Fast Forward <ffwd-txnx@users.noreply.github.com>
2021-08-31 12:08:33 +0300

    Update cases for August 31
    Source:
    
    https://www.lrt.lt/naujienos/lietuvoje/2/1481482/lietuvoje-patvirtinti-646-koronaviruso-atvejai-mire-6-zmones-visi-jie-buvo-nepasiskiepije

--- corona_predictions.py
+++ corona_predictions.py
@@ -102,7 +102,7 @@ DAILY_CASES = [
     287, 517, 576, 599, 548, 585, 434,
     356, 560, 576, 506, 537, 580, 407,
     314, 600, 649, 703, 545, 634, 396,
-    353,
+    353, 646,
 ]
 # previously, this calculation was done manually; an artifact of better days
 cumulative = 0
